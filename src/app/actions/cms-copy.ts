8fbf101552b51ede0c1781e2bd1284589ede5bb2
James Hannan <jim9112@gmail.com>
2024-11-19 17:14:38 -0500

    bring over logic and refactor

--- src/app/actions/cms-copy.ts
+++ src/app/actions/cms-copy.ts
@@ -1,5 +1,79 @@
-'use server'
-export async function copyCmsPages() {
+'use server';
+interface RequestOptions {
+  method: string;
+  headers: {};
+  body?: string;
+  redirect?: RequestRedirect | undefined;
+}
+// To do: single page options
+export const getAllPages = async (
+  token: string,
+  allDraft: boolean,
+  sitePage: boolean
+) => {
+  const pageType = sitePage ? 'site-pages' : 'landing-pages';
+  const requestOptions: RequestOptions = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await fetch(
+    `https://api.hubapi.com/cms/v3/pages/${pageType}`,
+    requestOptions
+  );
+  const data = await response.json();
+  const modifiedData = data.results.map(
+    (page: {
+      archivedAt: string | undefined;
+      state: string;
+      publishImmediately: boolean;
+    }) => {
+      const pageCopy = { ...page };
+      delete pageCopy.archivedAt;
+      if (allDraft) {
+        pageCopy.state = 'DRAFT';
+      }
+      return pageCopy;
+    }
+  );
+  return modifiedData;
+};
+
+export const addPages = async (token: string, pages: [], sitePage: boolean) => {
+  const pageType = sitePage ? 'site-pages' : 'landing-pages';
+  const raw = JSON.stringify({
+    inputs: pages,
+  });
+  const requestOptions: RequestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: raw,
+    redirect: 'follow',
+  };
+  const response = await fetch(
+    `https://api.hubapi.com/cms/v3/pages/${pageType}/batch/create`,
+    requestOptions
+  );
+  const message = await response.text();
+  return message;
+};
+
+export async function copyCmsPages(formData: FormData) {
+  const data = {
+    fromPortal: formData.get('fromPortal') || null,
+    toPortal: formData.get('toPortal') || null,
+    allDraft: formData.get('allDraft') ? true : false,
+    sitePage: formData.get('sitePage') ? true : false,
+  };
   // Code to copy CMS content
-  console.log('Copying CMS Pages');
+  // const {tokenOne, tokenTwo, allDraft, sitePage} = await request.json();
+  // const data = await getAllPages(tokenOne, allDraft, sitePage);
+  // const message = await addPages(tokenTwo, data, sitePage);
+  // return json(message);
+  // formData.get('fromPortal');
+  console.log(data);
 }
--- src/app/cms-tools/page.tsx
+++ src/app/cms-tools/page.tsx
@@ -4,16 +4,21 @@ export default function Page() {
   return (
     <div>
       <h1>CMS Tools</h1>
-      <div className='flex mb-3 justify-evenly'>
-        <div className='form-control'>
-          <label className='flex gap-1 cursor-pointer label'>
-            <span className='label-text'>Landing Pages</span>
-            <input type='checkbox' className='toggle toggle-accent' />
-            <span className='label-text'>Website Pages</span>
-          </label>
+      <form action={copyCmsPages} className='flex flex-col items-center gap-2'>
+        <div className='flex mb-3 justify-evenly'>
+          <div className='form-control'>
+            <label className='flex gap-1 cursor-pointer label'>
+              <span className='label-text'>Landing Pages</span>
+              <input
+                type='checkbox'
+                name='sitePage'
+                id='sitePage'
+                className='toggle toggle-accent'
+              />
+              <span className='label-text'>Website Pages</span>
+            </label>
+          </div>
         </div>
-      </div>
-      <form className='flex flex-col items-center gap-2'>
         <div className='flex justify-center gap-4'>
           <div className='flex flex-col'>
             <label className='text-center label-text' htmlFor='fromPortal'>
@@ -54,13 +59,7 @@ export default function Page() {
             </label>
           </div>
         </div>
-        <button
-          className='btn btn-outline btn-primary'
-          onClick={(e) => {
-            e.preventDefault();
-            copyCmsPages();
-          }}
-        >
+        <button className='btn btn-outline btn-primary' type='submit'>
           Get pages
         </button>
       </form>
