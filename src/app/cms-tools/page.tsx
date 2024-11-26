3911b4c6449ea1e79284124b7f1b538163b405f6
James Hannan <jim9112@gmail.com>
2024-11-26 11:12:12 -0500

    move bulk form to its own component

--- src/app/cms-tools/page.tsx
+++ src/app/cms-tools/page.tsx
@@ -2,6 +2,7 @@
 import { copyCmsPages } from '../actions/cms-copy';
 import { useActionState } from 'react';
 import Modal from '../components/Modal';
+import BulkPageForm from './_components/BulkPageForm';
 
 const initialState = {
   message: '',
@@ -28,73 +29,7 @@ export default function Page() {
           role='tabpanel'
           className='tab-content bg-base-100 border-base-300 rounded-box p-6'
         >
-          <form
-            action={formAction}
-            className='flex flex-col items-center gap-2'
-          >
-            <div className='flex mb-3 justify-evenly'>
-              <div className='form-control'>
-                <label className='flex gap-1 cursor-pointer label'>
-                  <span className='label-text'>Landing Pages</span>
-                  <input
-                    type='checkbox'
-                    name='sitePage'
-                    id='sitePage'
-                    className='toggle toggle-accent'
-                  />
-                  <span className='label-text'>Website Pages</span>
-                </label>
-              </div>
-            </div>
-            <div className='flex justify-center gap-4'>
-              <div className='flex flex-col'>
-                <label className='text-center label-text' htmlFor='fromPortal'>
-                  From Portal
-                </label>
-                <input
-                  required
-                  type='text'
-                  placeholder='Portal Private App Key'
-                  className='input input-bordered w-full max-w-xs'
-                  name='fromPortal'
-                  id='fromPortal'
-                />
-              </div>
-              <div className='flex flex-col'>
-                <label className='text-center label-text' htmlFor='toPortal'>
-                  To Portal
-                </label>
-                <input
-                  required
-                  type='text'
-                  placeholder='Portal Private App Key'
-                  className='input input-bordered w-full max-w-xs'
-                  name='toPortal'
-                  id='toPortal'
-                />
-              </div>
-              <div className='form-control'>
-                <label
-                  className='flex flex-col p-0 cursor-pointer label'
-                  htmlFor='allDraft'
-                >
-                  <span className='label-text'> Import all as draft </span>
-                  <input
-                    className='checkbox checkbox-primary'
-                    type='checkbox'
-                    name='allDraft'
-                    id='allDraft'
-                  />
-                </label>
-              </div>
-            </div>
-            <button
-              disabled={isPending}
-              className='btn btn-outline btn-primary'
-            >
-              Get pages
-            </button>
-          </form>
+          <BulkPageForm formAction={formAction} isPending={isPending} />
         </div>
 
         <input
