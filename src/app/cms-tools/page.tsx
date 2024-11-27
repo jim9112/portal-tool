753abd5db3c335ca8850ad0774a12c7d54fa6c9e
James Hannan <jim9112@gmail.com>
2024-11-27 18:28:25 -0500

    save portal keys to local storage

--- src/app/cms-tools/page.tsx
+++ src/app/cms-tools/page.tsx
@@ -1,12 +1,45 @@
 'use client';
+import { useState } from 'react';
+import Input from '../components/Input';
 import Tab from '../components/Tab';
 import BulkPageForm from './_components/BulkPageForm';
 import SinglePagesTab from './_components/SinglePagesTab';
 export default function Page() {
+  interface FormEvent extends React.FormEvent<HTMLFormElement> {}
+  const [portalKeys, setPortalKeys] = useState({
+    fromPortal: '',
+    toPortal: '',
+  });
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+    localStorage.setItem('portalKeys', JSON.stringify(portalKeys));
+  };
   return (
     <div className='container mx-auto font-body pt-20'>
       <h1 className='text-3xl font-heading text-center mb-10'>CMS Tools</h1>
-
+      <form className='flex flex-col justify-center' onSubmit={handleSubmit}>
+        <div className='flex justify-center gap-4'>
+          <Input
+            onChangeCallback={(e) =>
+              setPortalKeys({ ...portalKeys, [e.target.name]: e.target.value })
+            }
+            name='fromPortal'
+            label='Starting Portal'
+          />
+          <Input
+            onChangeCallback={(e) =>
+              setPortalKeys({ ...portalKeys, [e.target.name]: e.target.value })
+            }
+            name='toPortal'
+            label='Destination Portal'
+          />
+        </div>
+        <div className='flex justify-center mt-4 mb-4'>
+          <button className='btn btn-primary' type='submit'>
+            Save
+          </button>
+        </div>
+      </form>
       <div role='tablist' className='tabs tabs-lifted'>
         <Tab name='my_tabs_3' label='Bulk Pages Import' defaultChecked>
           <BulkPageForm />
