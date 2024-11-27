f5df0de6c1721df8214dd8260cf11c2db8dfc8da
James Hannan <jim9112@gmail.com>
2024-11-27 16:22:37 -0500

    add pagination to table

--- src/app/cms-tools/_components/PagesTable.tsx
+++ src/app/cms-tools/_components/PagesTable.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 interface PagesTableProps {
   title: string;
   sitePageList: Array<any>;
@@ -14,6 +16,7 @@ export default function PagesTable({
   const copySinglePage = (page: { name: string }) => {
     triggerCopy(page, sitePage);
   };
+  const [pageNumber, setPageNumber] = useState(1);
   return (
     <div className='overflow-x-auto mt-7'>
       <h3 className='text-xl text-center'>{title}</h3>
@@ -29,29 +32,48 @@ export default function PagesTable({
         </thead>
         <tbody>
           {sitePageList.length > 0 &&
-            sitePageList.map((page, index) => (
-              <tr key={page.id}>
-                <th>
-                  <div className='flex items-center gap-3'>
-                    <div>
-                      <div className='font-bold'>{page.name}</div>
+            sitePageList
+              .slice(pageNumber * 5 - 5, pageNumber * 5)
+              .map((page, index) => (
+                <tr key={page.id}>
+                  <th>
+                    <div className='flex items-center gap-3'>
+                      <div>
+                        <div className='font-bold'>{page.name}</div>
+                      </div>
                     </div>
-                  </div>
-                </th>
-                <td>{page.currentState}</td>
-                <td>{page.url}</td>
-                <th>
-                  <button
-                    onClick={() => copySinglePage(page)}
-                    className='btn btn-primary btn-xs'
-                  >
-                    Copy Page
-                  </button>
-                </th>
-              </tr>
-            ))}
+                  </th>
+                  <td>{page.currentState}</td>
+                  <td>{page.url}</td>
+                  <th>
+                    <button
+                      onClick={() => copySinglePage(page)}
+                      className='btn btn-primary btn-xs'
+                    >
+                      Copy Page
+                    </button>
+                  </th>
+                </tr>
+              ))}
         </tbody>
       </table>
+      {Math.ceil(sitePageList.length / 5) > 1 && (
+        <div className='flex justify-center mt-4'>
+          <div className='join'>
+            {[...Array(Math.ceil(sitePageList.length / 5))].map((_, index) => (
+              <button
+                key={index}
+                onClick={() => setPageNumber(index + 1)}
+                className={`join-item btn ${
+                  index + 1 === pageNumber ? 'btn-active' : ''
+                }`}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
