f95b848f3d8661fe3f82972d5ccc9d0ba9332b74
James Hannan <jim9112@gmail.com>
2024-12-02 15:09:56 -0500

    testing finding images in page content

--- src/app/cms-tools/_components/SinglePagesTable.tsx
+++ src/app/cms-tools/_components/SinglePagesTable.tsx
@@ -16,6 +16,40 @@ export default function SinglePagesTable({
   const copySinglePage = (page: { name: string }) => {
     rowCta(page, sitePage);
   };
+  const importImages = (page: { name: string }) => {
+    function findNestedImages(obj: {}) {
+      const images: String[] = [];
+
+      function search(obj: any) {
+        // Check if the current object is indeed an object
+        if (obj !== null && (typeof obj === 'object' || Array.isArray(obj))) {
+          // If the object has the key "img", add it to the images array
+          if (typeof obj === 'object' && obj.hasOwnProperty('img')) {
+            // if images doesnt contain the image src, add it
+            if (!images.includes(obj.img.src)) {
+              images.push(obj.img.src);
+            }
+          }
+          // Search through all elements in the array
+          if (Array.isArray(obj)) {
+            obj.forEach((element) => search(element));
+          } else {
+            // Recursively search through all keys in the object
+            for (const key in obj) {
+              if (obj.hasOwnProperty(key)) {
+                search(obj[key]);
+              }
+            }
+          }
+        }
+      }
+
+      search(obj);
+      return images;
+    }
+    const images = findNestedImages(page);
+    console.log(images);
+  };
   const [pageNumber, setPageNumber] = useState(1);
   return (
     <div className='overflow-x-auto mt-7'>
@@ -27,6 +61,7 @@ export default function SinglePagesTable({
             <th>Page Name</th>
             <th>Page Status</th>
             <th>Url</th>
+            <th></th>
             <th></th>
           </tr>
         </thead>
@@ -45,14 +80,22 @@ export default function SinglePagesTable({
                   </th>
                   <td>{page.currentState}</td>
                   <td>{page.url}</td>
-                  <th>
+                  <td>
                     <button
                       onClick={() => copySinglePage(page)}
                       className='btn btn-primary btn-xs'
                     >
                       Copy Page
                     </button>
-                  </th>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => importImages(page)}
+                      className='btn btn-primary btn-xs'
+                    >
+                      Import Images
+                    </button>
+                  </td>
                 </tr>
               ))}
         </tbody>
